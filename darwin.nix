b462404f5a431fab135b379df18df651c82fea53
Nauval Atmaja <nauval.atmaja@gmail.com>
2026-09-06 05:17:11 +0700

    feat: sshd on startup

--- darwin.nix
+++ darwin.nix
@@ -4,6 +4,16 @@
   system.primaryUser = machine.home.username;
   system.stateVersion = 6;
 
+  services.openssh = {
+    enable = true;
+    extraConfig = ''
+      AuthenticationMethods publickey
+      PubkeyAuthentication yes
+      PasswordAuthentication no
+      KbdInteractiveAuthentication no
+    '';
+  };
+
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
